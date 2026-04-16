bbafccdd583944b13c3eec8077d8698be87e3b73
madbark67 <239074869+madbark67@users.noreply.github.com>
2026-04-15 22:54:02 -0400

    create row_check specs and method

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,17 @@
 class Board
+  ROWS = 6
+  COLS = 7
+
   attr_accessor :grid
 
   def initialize
-    @grid = Array.new(6) { Array.new(7, '-') }
+    @grid = Array.new(6) { Array.new(7) }
+  end
+
+  def row_check(row, icon)
+    grid[row].each_cons(4) do |four|
+      return true if four.all? { |value| value == icon }
+    end
+    false
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,10 +3,35 @@ require_relative '../lib/Board'
 RSpec.describe Board do
   describe '#initialize' do
     let(:board) { Board.new }
-    it 'creates a 6x7 array grid' do
+    it 'creates a 6x7 array grid with nil values' do
       expect(board.grid.length).to eql(6)
       expect(board.grid).to all(have_attributes(length: 7))
-      expect(board.grid).to all(all(eq('-')))
+      expect(board.grid).to all(all(be(nil)))
+    end
+  end
+
+  describe '#row_check' do
+    let(:board) { Board.new }
+    it 'checks if 4 in a row of the same icon returns true' do
+      board.grid[0][0..3] = %w[X X X X]
+
+      expect(board.row_check(0, 'X')).to be(true)
+    end
+
+    it 'checks if 4 in a row of the different icons returns false' do
+      board.grid[0][0..1] = %w[X X]
+      board.grid[0][2..3] = %w[O O]
+
+      expect(board.row_check(0, 'X')).to be(false)
+    end
+
+    it 'checks 4 of the same icons not in a row returns false' do
+      board.grid[0][0] = 'X'
+      board.grid[0][2] = 'X'
+      board.grid[0][3] = 'X'
+      board.grid[0][5] = 'X'
+
+      expect(board.row_check(0, 'X')).to be(false)
     end
   end
 end
